cb77d8a420b19474d11ae81bcc50ef9331184e31
Varunkashyap2303 <r.varunkashyap99@gmail.com>
2025-07-15 23:55:39 +1000

    Made tweaks to the design and font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 export default function Navbar() {
   return (
     <header className="w-full flex justify-between items-center py-6 max-w-7xl mx-auto px-6">
-      <div className="text-2xl font-bold text-white">
+      <div className="text-2xl font-roboto text-white">
         <img src="/Primary_logo.png" alt="MOSAIC logo" className="h-25" />
       </div>
       <nav className="space-x-8 text-white text-sm">
-        <a href="#team" className="hover:underline">Our team</a>
-        <a href="#projects" className="hover:underline">Projects</a>
-        <a href="#contact" className="hover:underline">Contact us</a>
-        <a href="#join" className="hover:underline">Join us</a>
+        <a href="#team" className="hover:underline font-roboto">Our team</a>
+        <a href="#projects" className="hover:underline font-roboto">Projects</a>
+        <a href="#contact" className="hover:underline font-roboto">Contact us</a>
+        <a href="#join" className="hover:underline font-roboto">Join us</a>
       </nav>
     </header>
   );
