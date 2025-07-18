7ab8323a86cb00ecddbf0b31e4a3480e807ae3d3
trietGoToTheZoo <trietbui2810@outlook.com>
2025-07-19 00:18:42 +1000

    Fix page highlight

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,10 @@ import { usePathname } from 'next/navigation';
 export default function Navbar({ color = 'light' }) {
   const textColor = color === 'dark' ? 'text-[#213359]' : 'text-white';
   const pathname = usePathname();
+  const hoverBorderWhite = "hover:border-white"
+  const hoverBorderNavy = "hover:border-[#213359]"
+  const commonBorderStyle = " hover:border-2 px-4 py-4 rounded-full transition-all duration-100"
+  const underlineStyle = " font-bold underline underline-offset-8 decoration-lime-400"
 
   // Define your logo sources based on routes
   const logoPaths = {
@@ -18,8 +22,16 @@ export default function Navbar({ color = 'light' }) {
     // Add more routes as needed
   };
 
+  const borderHoverStyle = {
+    '/': hoverBorderWhite,
+    '/join': hoverBorderNavy,
+    '/project': hoverBorderNavy,
+    '/contact': hoverBorderNavy,
+  }
+  // ${pathname === '/contact' ? 'font-bold underline underline-offset-8 decoration-lime-400' : ''
   // Fallback to primary logo if route not specified
   const logoSource = logoPaths[pathname] || '/Primary_logo.png';
+  const borderStyle = borderHoverStyle[pathname] + commonBorderStyle
   return (
     <header className="w-full flex justify-between items-center py-6 max-w-7xl mx-auto px-6">
       <div className={`text-2xl font-roboto ${textColor}`}>
@@ -28,10 +40,10 @@ export default function Navbar({ color = 'light' }) {
         </Link>
       </div>
       <nav className={`space-x-8 ${textColor} text-sm`}>
-        <Link href="#team" className="hover:border-white hover:border-2 px-4 py-4 rounded-full transition-all duration-200 ">Our team</Link>
-        <Link href="#projects" className="hover:border-white hover:border-2 px-4 py-4 rounded-full transition-all duration-200 ">Projects</Link>
-        <Link href="/contact" className={`hover:border-white hover:border-2 px-4 py-4 rounded-full transition-all duration-200  ${pathname === '/contact' ? 'font-bold underline underline-offset-8 decoration-lime-400' : ''}`}>Contact us</Link>
-        <Link href="#join" className="hover:border-white hover:border-2 px-4 py-4 rounded-full transition-all duration-200">Join us</Link>
+        <Link href="#team" className={borderStyle}>Our team</Link>
+        <Link href="#projects" className={borderStyle}>Projects</Link>
+        <Link href="/contact" className={borderStyle + `${pathname === '/contact' ? underlineStyle : ''}`}>Contact us</Link>
+        <Link href="#join" className={borderStyle}>Join us</Link>
       </nav>
     </header>
   );
