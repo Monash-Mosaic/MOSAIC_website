beef2ec8b3164dd7e6aa14f24b5dbe5886f1c17f
MinhTrietBui-hanoi <mbui0018@student.monash.edu>
2025-07-23 00:13:26 +1000

    Bugfix scroll (#13)
    * bugfix 1
    
    * bugfix2
    
    ---------
    
    Co-authored-by: trietGoToTheZoo <trietbui2810@outlook.com>

--- src/components/RecentProjects.jsx
+++ src/components/RecentProjects.jsx
@@ -17,21 +17,21 @@ export default function RecentProjects() {
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
       transition={{ duration: 0.8 }}
-      className="pt-50 min-h-screen bg-[#D6DEFF] py-30 px-6 text-center snap-start w-full"
+      className="pt-50  bg-[#D6DEFF] py-30 px-6 text-center snap-start w-full"
     >
 
       <h2 className="text-4xl md:text-4xl font-extrabold text-[#4953A1] mb-12">Recent projects</h2>
 
-      <div className="grid gap-10 grid-cols-1 md:grid-cols-3 max-w-8xl mx-auto">
+      <div className="grid gap-2 md:gap-10 grid-cols-1 md:grid-cols-3 max-w-8xl mx-auto">
         {projects.map((prj, i) => (
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.4, delay: prj.delay, damping: 10 }}
             key={i} className="rounded-lg p-6 max-w-6xl">
-            <div className="w-full h-80 flex items-center justify-center mb-4 p-6">
+            <div className="w-full h-50 md:h-70  flex items-center justify-center mb-4 p-6">
               <span className="text-4xl text-gray-500"></span>
-              <img src={prj.image} className="h-60 w-auto border-[#6D92E2]/50 rounded-lg shadow-md " />
+              <img src={prj.image} className=" h-50 md:h-60 w-auto border-[#6D92E2]/50 rounded-lg shadow-md " />
             </div>
             <h3 className="text-lg font-semibold text-gray-800 mb-2">{prj.title}</h3>
             <p className="text-sm text-gray-600">
